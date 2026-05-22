7a590ef4b3b71e11ccaa32ed780c8bc6bfd55954
akimpeople <87603033+akimpeople@users.noreply.github.com>
2026-05-22 19:30:21 +0500

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,4 +1,9 @@
-import { CONFIG } from "./config.js";
+const CONFIG = {
+  SUPABASE_URL: "https://izzqhixtzxkjiwyquvri.supabase.co",
+  SUPABASE_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml6enFoaXh0enhraml3eXF1dnJpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk0NTIwODEsImV4cCI6MjA5NTAyODA4MX0.5vrBFp2WTNFlxPkhuQgPDlhq5t20JZUETT3c_rD84aM",
+  WHATSAPP_PHONE: "77054959007",
+  ADMIN_PASSWORD: "4958"
+};
 
 const supabase = window.supabase.createClient(
   CONFIG.SUPABASE_URL,
